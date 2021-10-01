58d4b6bbf47775acf5596f6c910c603328e364fe
elkino7 <eoortizs@correo.udistrital.edu.co>
2021-10-01 15:43:31 -0500

    Agregar archivos de proyecto.

---
+++ BlazorLDAP/Controllers/LoginController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.DirectoryServices;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorLDAP.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost("/account/login")]
+        public async Task<IActionResult> Login(UserCredentials credentials)
+        {
+            //Indicamos el dominio en el que vamos a buscar al usuario
+            //string path = "LDAP://servidor.dominio.local/dc=servidor,dc=dominio,dc=local";
+            string path = "LDAP://10.3.0.13:389";
+            try
+            {
+                using (DirectoryEntry entry = new DirectoryEntry(path, credentials.Username, credentials.Password))
+                {
+                    using (DirectorySearcher searcher = new DirectorySearcher(entry))
+                    {
+                        //Buscamos por la propiedad SamAccountName
+                        searcher.Filter = "(samaccountname=" + credentials.Username + ")";
+                        //Buscamos el usuario con la cuenta indicada
+                        var result = searcher.FindOne();
+                        //Console.WriteLine(result.GetDirectoryEntry().Properties["displayname"].Value.ToString());
+                        //Console.WriteLine(credentials.Username);
+                        if (result != null)
+                        {
+                            string role = "";
+                            string title = "";
+                            string name = "";
+                            string PersonalPager = "";
+                            string CN = "";                           //Comporbamos las propiedades del usuario
+                            ArrayList grupos = new ArrayList();       //Grupos del usuario  
+                            //Comporbamos las propiedades del usuario
+                            ResultPropertyCollection fields = result.Properties;
+                            foreach (String ldapField in fields.PropertyNames)
+                            {
+                                foreach (Object myCollection in fields[ldapField])
+                                {
+                                    if (ldapField == "employeetype")
+                                    {
+                                        role = myCollection.ToString().ToLower();
+                                    }
+                                    if (ldapField == "displayname")
+                                    {
+                                        name = myCollection.ToString().ToUpper();
+                                        Console.WriteLine("ROL " + role.ToString());
+                                    }
+                                    if (ldapField == "title")
+                                    {
+                                        title = myCollection.ToString().ToUpper();
+                                        Console.WriteLine("CARGO " + title.ToString());
+                                    }
+                                    if (ldapField == "postalcode")
+                                    {
+                                        PersonalPager = myCollection.ToString().ToUpper();
+                                        Console.WriteLine("postalcode " + PersonalPager.ToString());
+                                    }
+                                    if (ldapField == "cn")
+                                    {
+                                        CN = myCollection.ToString().ToUpper();
+                                        Console.WriteLine("CN " + CN.ToString());
+                                        //Console.WriteLine("ROL "+ role.ToString());
+                                    }
+                                    if (ldapField == "title")
+                                    {
+                                        title = myCollection.ToString().ToUpper();
+                                        Console.WriteLine("CARGO" + title.ToString());
+                                    }
+                                }
+
+                            }
+
+                            //pruebas para encontrar grupo
+                            DirectorySearcher search = new DirectorySearcher(path);
+                            search.Filter = "(cn=" + CN + ")";
+                            search.PropertiesToLoad.Add("memberOf");
+                            int propertyCount = result.Properties["memberOf"].Count;
+                            string dn;
+                            int equalIndex, CommaIndex;
+
+                            for (int propertCounter = 0; propertCounter < propertyCount; propertCounter++)
+                            {
+                                dn = (string)result.Properties["memberOf"][propertCounter];
+                                equalIndex = dn.IndexOf("=", 1);
+                                CommaIndex = dn.IndexOf(",", 1);
+                                if (-1 == equalIndex) { break; }
+                                grupos.Add(dn.Substring((equalIndex + 1), (CommaIndex - equalIndex) - 1));
+                                Console.WriteLine();
+                            }
+                            //Pruebas para encontrar grupo
+                            //Añadimos los claims Usuario y Rol para tenerlos disponibles en la Cookie
+                            Console.WriteLine("Tamano Grupos : " + grupos.Count);
+
+
+                            //Añadimos los claims Usuario y Rol para tenerlos disponibles en la Cookie
+                            //Podríamos obtenerlos de una base de datos.
+                            var claims = new[]
+                            {
+                                new Claim(ClaimTypes.Name, name),
+                                new Claim(ClaimTypes.Role, role),
+                                new Claim(ClaimTypes.NameIdentifier,title),
+                                new Claim(ClaimTypes.Country,CN),
+                                new Claim(ClaimTypes.Dsa,PersonalPager),
+                                new Claim(ClaimTypes.Dns,(string)grupos[0])
+                            };
+
+                            //Creamos el principal
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                            //Generamos la cookie. SignInAsync es un método de extensión del contexto.
+                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+                            //Redirigimos a la Home
+                            return LocalRedirect("/");
+
+                        }
+                        else
+                            return LocalRedirect("/login/Invalid credentials");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return LocalRedirect("/login/Invalid credentials");
+            }
+        }
+
+        [HttpGet("/account/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/");
+        }
+    }
+
+    public class UserCredentials
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
---
+++ BlazorLDAP/Startup.cs
@@ -0,0 +1,70 @@
+using BlazorLDAP.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLDAP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            //Nuevos servicios
+            services.AddControllers();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();//Cookies para mantener el estado de autenticación
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            //Agregar los Midlewares para gestionar la authenticación y la autorización
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                //Enponit para ejecutar las rutas que vamos a agregar.
+                endpoints.MapControllers();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
